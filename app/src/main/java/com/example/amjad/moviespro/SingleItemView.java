41f07d766e4906bf03f43f8e3e1e70beed7143d7
Hanan Amjed <hanonabest@hotmail.com>
2016-08-16 16:43:20 +0300

    resized size of image on pressing item

--- app/src/main/java/com/example/amjad/moviespro/SingleItemView.java
+++ app/src/main/java/com/example/amjad/moviespro/SingleItemView.java
@@ -2,18 +2,20 @@ package com.example.amjad.moviespro;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.InputStream;
 
 public class SingleItemView extends Activity {
     String id;
     String title;
     String vote_average;
     String m_image;
-    String position;
-   // String url = "http://image.tmdb.org/t/p/w780"+m_image;
-    //ImageLoader imageLoader = new ImageLoader(this);
 
 
     @Override
@@ -24,20 +26,47 @@ public class SingleItemView extends Activity {
         id = i.getStringExtra("id");
         title = i.getStringExtra("title");
         vote_average = i.getStringExtra("vote_average");
-        m_image = i.getStringExtra("poster_path");
+        m_image = i.getStringExtra("m_image");
         
         TextView txtid = (TextView) findViewById(R.id.id);
         TextView txttitle = (TextView) findViewById(R.id.title);
         TextView txtvote_average = (TextView) findViewById(R.id.vote_average);
         ImageView imgflag = (ImageView) findViewById(R.id.ivImage);
 
-        Movies movies = new Movies();
-        imgflag.setImageResource(R.mipmap.ic_launcher);
-        DownloadImage downloadImage = (DownloadImage) new DownloadImage(imgflag).execute(movies.get_M_image());
+
+        imgflag.setImageResource(R.drawable.ic_launcher);
+        new DownloadImageTask(imgflag).execute(m_image);
         txtid.setText(id);
         txttitle.setText(title);
         txtvote_average.setText(vote_average);
 
 
     }
+
+
+private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+    ImageView bmImage;
+
+    public DownloadImageTask(ImageView bmImage) {
+        this.bmImage = bmImage;
+    }
+
+    protected Bitmap doInBackground(String... urls) {
+        String urldisplay = urls[0];
+        Bitmap mIcon11 = null;
+        try {
+            InputStream in = new java.net.URL(urldisplay).openStream();
+            mIcon11 = BitmapFactory.decodeStream(in);
+        } catch (Exception e) {
+            //  Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+        return mIcon11;
+    }
+
+    protected void onPostExecute(Bitmap result) {
+        bmImage.setImageBitmap(result);
+    }
+
+}
 }
